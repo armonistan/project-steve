cb54bc2cba06c045c0cb5746c5599b4394d924a8
dpumford <dpumford@hotmail.com>
2014-07-14 15:57:29 -0500

    -Made boss summons stick around.

--- Steve/src/com/steve/Snake.java
+++ Steve/src/com/steve/Snake.java
@@ -305,7 +305,7 @@ public class Snake {
 	}
 	
 	public void addMoney(int amount) {
-		money += amount * SteveDriver.constants.get("goldModifier") * 100000; //TODO: CHANGE BACK
+		money += amount * SteveDriver.constants.get("goldModifier"); //TODO: CHANGE BACK
 		
 		//TODO: Make this only save when needed.
 		SteveDriver.prefs.putInteger("money", money);
