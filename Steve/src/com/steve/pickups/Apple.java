650a62deac77b98d2216160743048aa2ec7950cb
armon <anayerai@uark.edu>
2014-07-15 17:49:30 -0500

    buffed tier3 laser

--- Steve/src/com/steve/pickups/Apple.java
+++ Steve/src/com/steve/pickups/Apple.java
@@ -9,7 +9,7 @@ import com.steve.base.Pickup;
 public class Apple extends Pickup {
 	
 	public Apple(float x, float y) {
-		super(x, y, 4 * SteveDriver.TEXTURE_SIZE, 0, 175);
+		super(x, y, 4 * SteveDriver.TEXTURE_SIZE, 0, 17500000);
 		int soundDecider = SteveDriver.random.nextInt(3)+1;
 		pickupSound = Gdx.audio.newSound(Gdx.files.internal("audio/eatApple" + soundDecider + ".ogg"));
 	}
